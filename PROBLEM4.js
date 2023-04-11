a4622232bfa4f387f1cfa73ad6986a81ab6f0f37
ritesh <riteshkashyap9999@gmail.com>
2023-04-12 00:50:06 +0530

    probelm 4

---
+++ PROBLEM4.js
@@ -0,0 +1,8 @@
+let name="Ritesh",school="KV",grade="12th",section="A";
+let rollno=110,hindi=90 , english=95 , maths=90;
+
+console.log(name," ",school," ",grade," ",section);
+console.log("Hindi = ",hindi);
+console.log("English = ",english);
+console.log("Maths = ",maths);
+console.log("Total = ",maths+english+hindi);
