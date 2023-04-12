ec54de61db96278d8c1babe60c5802d20a267095
ritesh <riteshkashyap9999@gmail.com>
2023-04-13 01:40:06 +0530

    Array Problem

---
+++ problem6.js
@@ -0,0 +1,15 @@
+let N=0;
+ 
+    if(Math.floor(32/N)==0)
+    {
+      console.log((N/32));
+        console.log("Too Low");
+    }
+    else if(N/32>0)
+    {
+        console.log(Math.floor(32/N));
+    }
+    else
+    {
+        console.log(-1);
+    }
